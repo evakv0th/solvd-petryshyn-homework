f38d985d9962a4de55b55f9296cf96685c3aa14c
kvothe eva <evakv0th@gmail.com>
2023-09-14 16:22:50 +0300

    made customhash external function, not method of hashtable

--- index.js
+++ index.js
@@ -11,48 +11,9 @@
 // it was using external object, but with hashTable class its easier
 // if there is key already in hashTable but value is different, than its adding "x" to salt
 // this ensures that for same string it will always give same output and for collisions it will take another string
-
 class HashTable {
   constructor() {
     this.table = {};
-  }
-
-  customHash(string, salt = "salt") {
-    if (typeof string !== "string") {
-      return new Error("provide string");
-    }
-    const inputCombined = string + salt;
-
-    let hashResult = 0;
-
-    let checkerForThirdIteration = 0;
-    for (let i = 0; i < inputCombined.length; i++) {
-      if (checkerForThirdIteration < 2) {
-        hashResult += parseInt(
-          inputCombined.charCodeAt(i) * inputCombined.length
-        );
-        checkerForThirdIteration++;
-      } else {
-        checkerForThirdIteration = 0;
-        hashResult += parseInt(
-          inputCombined.charCodeAt(i) *
-            inputCombined.length *
-            inputCombined.length
-        );
-      }
-    }
-    const result = hashResult;
-
-    if (
-      this.table.hasOwnProperty(result) &&
-      this.table[result] !== inputCombined
-    ) {
-      let newSalt = salt + "x";
-      return this.customHash(string, newSalt);
-    } else {
-      this.table[result] = inputCombined;
-      return result;
-    }
   }
 
   add(key, value) {
@@ -69,22 +30,56 @@ class HashTable {
 
 const hashTable = new HashTable();
 
-console.log(hashTable.customHash("at me 2"));
-console.log(hashTable.customHash("bt le 2")); // first 2 cases produces same output without collision handling, you can check it and comment out 'at me 2' and this line will output 28413
-console.log(hashTable.customHash("at me 2", "saltttt"));
+function customHash(string, salt = "salt") {
+  if (typeof string !== "string") {
+    return new Error("provide string");
+  }
+  const inputCombined = string + salt;
+
+  let hashResult = 0;
+
+  let checkerForThirdIteration = 0;
+  for (let i = 0; i < inputCombined.length; i++) {
+    if (checkerForThirdIteration < 2) {
+      hashResult += parseInt(
+        inputCombined.charCodeAt(i) * inputCombined.length
+      );
+      checkerForThirdIteration++;
+    } else {
+      checkerForThirdIteration = 0;
+      hashResult += parseInt(
+        inputCombined.charCodeAt(i) *
+          inputCombined.length *
+          inputCombined.length
+      );
+    }
+  }
+  const result = hashResult;
+
+  if (
+    hashTable.table.hasOwnProperty(result) &&
+    hashTable.table[result] !== inputCombined
+  ) {
+    let newSalt = salt + "x";
+    return customHash(string, newSalt);
+  } else {
+    hashTable.table[result] = inputCombined;
+    return result;
+  }
+}
+
+console.log(customHash("at me 2"));
+console.log(customHash("bt le 2")); // first 2 cases produces same output without collision handling, you can check it and comment out 'at me 2' and this line will output 28413
+console.log(customHash("at me 2", "saltttt"));
+console.log(customHash("youAreDoingVeryWellImVeryProudOfYou", "gabe"));
+
+console.log(hashTable.get(customHash("at me 2")));
+console.log(hashTable.get(customHash("bt le 2")));
+console.log(hashTable.get(customHash("at me 2", "saltttt")));
 console.log(
-  hashTable.customHash("youAreDoingVeryWellImVeryProudOfYou", "gabe")
+  hashTable.get(customHash("youAreDoingVeryWellImVeryProudOfYou", "gabe"))
 );
 
-console.log(hashTable.get(hashTable.customHash("at me 2")));
-console.log(hashTable.get(hashTable.customHash("bt le 2")));
-console.log(hashTable.get(hashTable.customHash("at me 2", "saltttt")));
-console.log(
-  hashTable.get(
-    hashTable.customHash("youAreDoingVeryWellImVeryProudOfYou", "gabe")
-  )
-);
-
-hashTable.remove(hashTable.customHash("at me 2"));
+hashTable.remove(customHash("at me 2"));
 
 console.log(hashTable.table);
